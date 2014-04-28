2015ad27e403da882f1534099c720ae286817d5a
Zach Muscat <zachmbarca@gmail.com>
2014-04-28 22:10:50 +0200

    adding player file
    Signed-off-by: Zach Muscat <zachmbarca@gmail.com>

---
+++ src/main/java/com/pest/demo/Player.java
@@ -0,0 +1,137 @@
+package com.test.demo;
+
+import com.test.demo.Map.Colour;
+
+import java.util.ArrayList;
+
+public class Player
+{
+	private Position startingPosition, position;
+	private ArrayList<Position> trail = new ArrayList<Position>();
+	
+	/**
+	 * Assigns starting position to player, tile colour must be green
+	 * Adds starting position to trail
+	 */
+	
+	public void setStartingPosition(Map m)
+	{
+		int x, y;	
+		do
+		{
+			x = (int)(Math.random()*(m.getSize()-1))+1;
+			y = (int)(Math.random()*(m.getSize()-1))+1;
+		} while (m.getTiles()[x][y] != Map.Colour.GREEN);
+			
+		startingPosition = new Position(x, y);		
+		position = new Position(x, y);		
+		
+		trail.add(new Position(x, y));
+	}
+	
+	/** 
+	 * Moves Player
+	 * Returns false if move out of bounds, else moves player and returns true 
+	 */	
+	public boolean move(char direction, int bounds)
+	{
+		switch(direction)
+		{
+			case 'U':
+			case 'u':
+					if((position.getR()-1) >= 0) //check bounds			
+					{
+						position.setR(position.getR()-1);
+						return true;
+					}
+					else
+					{
+						return false;
+					}					
+			case 'D':
+			case 'd':
+					if((position.getR()+1) <= (bounds-1))
+					{
+						position.setR(position.getR()+1);
+						return true;
+					}
+					else
+					{
+						return false;
+					}					
+			case 'L':
+			case 'l':
+					if((position.getC()-1) >= 0)
+					{	
+						position.setC(position.getC()-1);
+						return true;			
+					}	
+					else
+					{
+						return false;
+					}					
+			case 'R':
+			case 'r':
+					if((position.getC()+1) <= (bounds-1))
+					{			
+						position.setC(position.getC()+1);
+						return true;			
+					}
+					else
+					{
+						return false;
+					}
+			default:
+					return false;
+		}
+	}	
+	
+	public ArrayList<Position> getTrail()
+	{		
+		return trail;		
+	}
+	
+	/**
+	 * If not already in trail, adds current position to trail
+	 */
+	public void updateTrail()
+	{
+		if(!trail.contains(position))
+			trail.add(new Position(position.getR(), position.getC()));
+	}
+	
+	public Position getStartingPosition()
+	{
+		return startingPosition;
+	}	
+	
+	/**
+	 * returns the colour of current position of player	
+	 */
+	public Colour checkColour(Map map)
+	{
+		return map.getTiles()[position.getR()][position.getC()]; 					
+	}		
+	
+	/**
+	 * Moves player back to starting position
+	 */
+	public void moveBackToStart(int num)
+	{
+		System.out.println("Player " + num + " Stepped in Water! Resetting to original position...");
+		position.setR(startingPosition.getR());
+		position.setC(startingPosition.getC());
+	}
+	
+	public int getPositionR()
+	{
+		return position.getR();
+	}
+	
+	public int getPositionC()
+	{
+		return position.getC();
+	}	
+		
+	
+}
